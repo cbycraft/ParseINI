4a191af916191e6e637466cde3a52b0a4e0831f3
Carl Bycraft <carlbycraft@gmail.com>
2016-10-29 16:28:54 -0700

    Partial implementation of the Ini file read

--- ParseIni/Lexer.cs
+++ ParseIni/Lexer.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace ParseIni
 {
@@ -12,6 +9,12 @@ namespace ParseIni
         {
 
         }
+
+        public Lexer(string[] fileAsStringArray)
+        {
+
+        }
+
         internal enum Tokens
         {
             String,
@@ -19,6 +22,47 @@ namespace ParseIni
             CloseSquareBrace,
             EqualSign,
             Unknown
+        }
+
+        private class IniFile
+        {
+            private string[] fileAsArrayOfStrings = null;
+
+            public IniFile(string fullAbsolutePathToIniFile)
+            {
+                try
+                {
+                    this.fileAsArrayOfStrings = File.ReadAllLines(fullAbsolutePathToIniFile);
+                }
+                catch (ArgumentException argumentException)
+                {
+                    Console.WriteLine(argumentException.Message);
+                }
+                catch (IOException ioException)
+                {
+                    Console.WriteLine(ioException.Message);
+                }
+                catch (UnauthorizedAccessException unauthorizedAccessException)
+                {
+                    Console.WriteLine(unauthorizedAccessException.Message);
+                }
+                catch (NotSupportedException notSupportedException)
+                {
+                    Console.WriteLine(notSupportedException.Message);
+                }
+                catch (System.Security.SecurityException securityException)
+                {
+                    Console.WriteLine(securityException.Message);
+                }
+            }
+
+            public string[] FileAsArrayOfStrings
+            {
+                get
+                {
+                    return this.fileAsArrayOfStrings;
+                }
+            }
         }
     }
 }
