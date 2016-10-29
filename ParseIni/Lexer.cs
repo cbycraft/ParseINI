de7cadc92b9bb1149c2a306b18dce38c3b5be449
Carl Bycraft <carlbycraft@gmail.com>
2016-10-29 15:47:25 -0700

    Initial Checkin

---
+++ ParseIni/Lexer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseIni
+{
+    public class Lexer
+    {
+        public Lexer(string fullAbsolutePathToIniFile)
+        {
+
+        }
+        internal enum Tokens
+        {
+            String,
+            OpenSquareBrace,
+            CloseSquareBrace,
+            EqualSign,
+            Unknown
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ParseINI
+Parses INI file and returns a dictionary of dictionary indexed by first and second level key values
