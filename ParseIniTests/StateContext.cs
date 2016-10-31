aa4daa025c779d288c3adc8397c972b82a624a74
Carl Bycraft <carlbycraft@gmail.com>
2016-10-30 18:30:39 -0700

    Started implementing state pattern

---
+++ ParseIniTests/StateContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseIniTests
+{
+    public class StateContext
+    {
+        public StateContext(IState state)
+        {
+            State = state;
+        }
+
+        public IState State { get; set; }
+
+        public void StateRequest()
+        {
+            State.StateChange(this);
+        }
+    }
+}
---
+++ ParseIniTests/StateLexerCharacter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseIniTests
+{
+    public class StateLexerCharacter : IState
+    {
+        public void StateChange(StateContext stateContext)
+        {
+
+        }
+    }
+}
